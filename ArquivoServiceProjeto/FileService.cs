3ce9a23756c1b98621cd037d4f98decd12df85dc
StellaOliveiraM <smoreira.works@bancomaster.com.br>
2024-04-25 16:36:12 -0300

    injeção de dependencia

--- ArquivoServiceProjeto/FileService.cs
+++ ArquivoServiceProjeto/FileService.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using Consumo.Domain.Interfaces.Services;
 using FileShare.Interface;
 
--- ArquivoServiceProjeto/Program.cs
+++ ArquivoServiceProjeto/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Consumo.Domain.Interfaces.Services;
+using Consumo.Service.Services;
+using FileShare.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddSingleton<IFileService, FileService>();
+        builder.Services.AddFileShare();
+
+        var app = builder.Build();
+        app.Run();
+    }
+}
