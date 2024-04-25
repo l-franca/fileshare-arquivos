acac69004ff75c414f24e42e9a8523b3bf61fbf8
Leandro Guilherme Lacerda França <lfranca.works@bancomaster.com.br>
2024-04-25 16:08:37 -0300

    fileshare

---
+++ IFileShareService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Consumo.Domain.Interfaces.Services;
+
+public interface IFileShareService
+{
+    string BuscaNomeArquivo();
+    byte[] BuscaArquivo(string nomeArquivo);
+    bool EscreveArquivoEntradaProcessados(string nomeArquivo, MemoryStream arquivo);
+    void EscreveArquivoSaida(string nomeArquivo, MemoryStream arquivo);
+    List<string> ListaArquivosPastaSaida();
+    void DeletarArquivo(string nomeArquivo);
+}
