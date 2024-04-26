9513094f4fe18f5ac5bf29dc5a9caab285053e26
StellaOliveiraM <smoreira.works@bancomaster.com.br>
2024-04-26 13:04:58 -0300

    melhorias finais de mnu

---
+++ ArquivoServiceProjeto/OpcoesMenu.cs
@@ -0,0 +1,14 @@
+namespace ArquivoServiceProjeto;
+
+public enum OpcoesMenu
+{
+    ListarArquivosPastaRaiz = 1,
+    ListarArquivosPastaProcessados = 2,
+    ListarArquivosPastaSaida = 3,
+    BuscarArquivoPastaRaiz = 4,
+    BuscarArquivoPastaProcessados = 5,
+    BuscarArquivoPastaSaida = 6,
+    InserirArquivoPastaRaiz = 7,
+    DeletarArquivoPastaRaiz = 8,
+    Encerrar = 0
+}
--- ArquivoServiceProjeto/Processar.cs
+++ ArquivoServiceProjeto/Processar.cs
@@ -14,89 +14,96 @@ namespace ArquivoServiceProjeto
             _fileConfigurations = new GlobalConfigurations();
         }
 
+        private void MostrarOpcoes()
+        {
+            Console.Clear();
+            Console.WriteLine("Escolha uma das opções abaixo: ");
+            foreach (OpcoesMenu opcao in Enum.GetValues(typeof(OpcoesMenu)))
+            {
+                Console.WriteLine($"{(int)opcao} - {opcao}");
+            }
+        }
+
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var key = string.Empty;
+            var opcaoSelecionada = OpcoesMenu.Encerrar;
 
-            while (key != "0")
+            while (opcaoSelecionada != OpcoesMenu.Encerrar)
             {
-                Console.Clear();
-                Console.WriteLine("Escolha uma das opções abaixo: ");
-                Console.WriteLine("1 - Listar Arquivos na Pasta Raiz");
-                Console.WriteLine("2 - Listar Arquivos na Pasta Processados");
-                Console.WriteLine("3 - Listar Arquivos na Pasta Saida");
-                Console.WriteLine("4 - Buscar Arquivo da Pasta Raiz");
-                Console.WriteLine("5 - Buscar Arquivo da Pasta Processados");
-                Console.WriteLine("6 - Buscar Arquivo da Pasta Saida");
-                Console.WriteLine("7 - Inserir Arquivo na Pasta Raiz");
-                Console.WriteLine("8 - Deletar Arquivo na Pasta Raiz");
-                Console.WriteLine("0 - Encerrar");
-                key = Console.ReadLine();
+                MostrarOpcoes();
 
-                List<string> arquivos;
-                switch (key)
+                if (Enum.TryParse(Console.ReadLine(), out OpcoesMenu opcao))
                 {
-                    case "1":
-                        arquivos = _fileService.ListaArquivosPastaRaiz();
-                        Console.Clear();
-                        Console.WriteLine("Arquivos existentes na pasta:");
-                        foreach (var item in arquivos)
-                        {
-                            Console.WriteLine(item);
-                        }
+                    opcaoSelecionada = opcao;
 
-                        Console.ReadLine();
-                        break;
-
-                    case "2":
-                        arquivos = _fileService.ListaArquivosPastaProcessados();
-                        Console.Clear();
-                        Console.WriteLine("Arquivos existentes na pasta:");
-                        foreach (var item in arquivos)
-                        {
-                            Console.WriteLine(item);
-                        }
-
-                        Console.ReadLine();
-                        break;
-
-                    case "3":
-                        arquivos = _fileService.ListaArquivosPastaSaida();
-                        Console.Clear();
-                        Console.WriteLine("Arquivos existentes na pasta:");
-
-                        foreach (var item in arquivos)
-                        {
-                            Console.WriteLine(item);
-                        }
-
-                        Console.ReadLine();
-                        break;
-
-                    case "6":
-                        arquivos = _fileService.ListaArquivosPastaSaida();
-                        Console.Clear();
-                        foreach (var item in arquivos)
-                        {
-                            if (item.Length <= 4) continue;
-
-                            var arquivoByte = _fileService.BuscaArquivoSaida(item);
-                            var memoryStream = FileUtils.ConvertBytesToStream(arquivoByte);
-                            FileUtils.DownloadFile(memoryStream, item, _fileConfigurations.CaminhoEArquivoSaida);
-                        }
-
-                        break;
-                    case "7":
-                        var created = _fileService.EscreveArquivoRaiz(_fileConfigurations.NomeArquivoComExtensao, _fileConfigurations.StreamArquivo);
-                        Console.WriteLine(created ? "Arquivo criado no diretório" : "Erro na criação do arquivo");
-                        break;
-                    default:
-                        Console.WriteLine("Opção inválida.");
-                        break;
+                    switch (opcaoSelecionada)
+                    {
+                        case OpcoesMenu.ListarArquivosPastaRaiz:
+                            ListArquivosPasta(_fileService.ListaArquivosPastaRaiz);
+                            break;
+                        case OpcoesMenu.ListarArquivosPastaProcessados:
+                            ListArquivosPasta(_fileService.ListaArquivosPastaProcessados);
+                            break;
+                        case OpcoesMenu.ListarArquivosPastaSaida:
+                            ListArquivosPasta(_fileService.ListaArquivosPastaSaida);
+                            break;
+                        case OpcoesMenu.BuscarArquivoPastaSaida:
+                            ProcessarArquivosSaida();
+                            break;
+                        case OpcoesMenu.InserirArquivoPastaRaiz:
+                            InserirArquivoPastaRaiz();
+                            break;
+                        case OpcoesMenu.BuscarArquivoPastaRaiz:
+                        case OpcoesMenu.BuscarArquivoPastaProcessados:
+                        case OpcoesMenu.DeletarArquivoPastaRaiz:
+                        default:
+                            Console.WriteLine("Opção inválida.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Opção inválida. Por favor, selecione uma opção válida.");
                 }
             }
 
             return Task.CompletedTask;
+        }
+
+        private static void ListArquivosPasta(Func<List<string>> listarArquivosFunction)
+        {
+            var arquivos = listarArquivosFunction();
+
+            Console.Clear();
+            Console.WriteLine("Arquivos existentes na pasta:");
+
+            foreach (var arquivo in arquivos)
+            {
+                Console.WriteLine(arquivo);
+            }
+
+            Console.ReadLine();
+        }
+
+        private void ProcessarArquivosSaida()
+        {
+            var arquivos = _fileService.ListaArquivosPastaSaida();
+            Console.Clear();
+
+            foreach (var arquivo in arquivos)
+            {
+                if (arquivo.Length <= 4) continue;
+
+                var arquivoByte = _fileService.BuscaArquivoSaida(arquivo);
+                var memoryStream = FileUtils.ConvertBytesToStream(arquivoByte);
+                FileUtils.DownloadFile(memoryStream, arquivo, _fileConfigurations.CaminhoEArquivoSaida);
+            }
+        }
+
+        private void InserirArquivoPastaRaiz()
+        {
+            var criado = _fileService.EscreveArquivoRaiz(_fileConfigurations.NomeArquivoComExtensao, _fileConfigurations.StreamArquivo);
+            Console.WriteLine(criado ? "Arquivo criado no diretório" : "Erro na criação do arquivo");
         }
     }
 }
